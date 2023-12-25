3f05f8a46e69199ce32f8af8f3e613b58e3619ff
roi-mashiah <roimash97@gmail.com>
2023-12-25 02:18:04 +0200

    added an option to train on ch type

--- helpers/data_utils.py
+++ helpers/data_utils.py
@@ -3,8 +3,13 @@ import os
 import numpy as np
 
 
-def concat_all_csv_files(data_path, group_size):
-    all_csv_file_paths = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.csv')]
+def concat_all_csv_files(data_path, group_size, model_type=None):
+    all_csv_file_paths = [os.path.join(data_path, f)
+                          for f in os.listdir(data_path)
+                          if f.endswith('.csv') and model_type in f] if model_type is not None \
+        else [os.path.join(data_path, f)
+              for f in os.listdir(data_path)
+              if f.endswith('.csv')]
     all_data_list = []
     for file in all_csv_file_paths:
         scenario = pd.read_csv(file)
@@ -21,9 +26,9 @@ def reshape_vectors_to_matrices(x, y, group_size):
     return x_r, y_r
 
 
-# if __name__ == '__main__':
-#     data_path = r"C:\Users\97252\PycharmProjects\CleaningData\data"
-#     input_data = concat_all_csv_files(data_path, 18)
-#
-#     group_1_samples = input_data[input_data['group'] == 1]
-#     print(group_1_samples.head(20))
+if __name__ == '__main__':
+    data_path = r"C:\Users\97252\PycharmProjects\CleaningData\data"
+    input_data = concat_all_csv_files(data_path, 18)
+
+    group_1_samples = input_data[input_data['group'] == 1]
+    print(group_1_samples.head(20))
