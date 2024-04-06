93e98e4af18e6287ed02042317f8818cf2929d4a
roi-mashiah <roimash97@gmail.com>
2024-04-06 15:57:16 +0300

    stable version for linear nn solution (config_K)

--- helpers/performance_plots.py
+++ helpers/performance_plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from torch import FloatTensor
 
 
@@ -12,22 +13,42 @@ def plot_loss(epochs, losses, title_str):
     plt.show()
 
 
+def get_ccdf(data: np.ndarray, N=10000):
+    # % This function plots the CCDF of the columns of data
+    ccdfs = []
+    for column_index in range(data.shape[-1]):
+        h, x = np.histogram(data[:, column_index], N)
+        my_pdf = h / data.shape[0]
+        my_cdf = np.cumsum(my_pdf)
+        my_ccdf = 1 - my_cdf
+        ccdfs.append({'ccdf': my_ccdf, 'x': 10 * np.log10(x[1:])})
+        # semilogy(x,my_ccdf)
+        # addFigNames('CCDF','alpha','P(x>alpha)',1,legendCell)
+    return ccdfs if len(ccdfs) > 1 else ccdfs[0]
+
+
 def plot_performance(results_df, args):
     writer, config_name = args
     for snr, snr_sub_df in results_df.groupby('snr'):
         i = 0
-        f = plt.figure(figsize=(10, 10))
+        f = plt.figure(figsize=(15, 12))
         for channel, ch_sub_df in snr_sub_df.groupby('ch'):
-            title = f"Channel {channel}, N={ch_sub_df.shape[0]}"
+            nn_ccdf = get_ccdf((ch_sub_df.nn_loss / ch_sub_df.bl_loss).values.reshape(ch_sub_df.shape[0], 1))
+            change_index = np.argwhere(nn_ccdf['x'] > 0)[0]
+            losing_probability = nn_ccdf['ccdf'][change_index]
+            title = f"Channel {channel} N={ch_sub_df.shape[0]} Pr(BL<NN) = {round(losing_probability[0], 3)}"
             plt.subplot(2, 3, i + 1, title=title)
-            plt.ecdf(ch_sub_df.nn_loss, label="neural net")
-            plt.ecdf(ch_sub_df.bl_loss, label="baseline")
+            plt.semilogy(nn_ccdf['x'], nn_ccdf['ccdf'], label="DB(NN loss/BL Loss)")
+            plt.ylim([10e-4, 1])
             plt.grid()
             plt.legend()
             i += 1
         writer.add_figure(f"Perf Plots - SNR {int(snr)}, Configuration {config_name}",
                           figure=f,
                           close=True)
+        # f.suptitle(f"SNR: {snr}")
+        # f.savefig(f"performance_figs\Configuration_M_SNR_{int(snr)}.png")
+    plt.show()
 
 
 def plot_channel_reconstruction(gt: FloatTensor, estimation: FloatTensor, baseline_channel_est, metadata, writer):
@@ -54,4 +75,6 @@ def plot_channel_reconstruction(gt: FloatTensor, estimation: FloatTensor, baseli
 
 
 if __name__ == '__main__':
+    res = pd.read_csv(r"..\helpers\ch_est_results_config_M.csv")
+    plot_performance(res, [None, ""])
     pass
